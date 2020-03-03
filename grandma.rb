acb543395ff80f115b2b57ae1cfb956b55bc9026
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-03 13:56:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 def speak_to_granda(statement)
-  if statement.upcase== true
-    puts "NO, NOT SINCE 1938!"
-  elseif statement= "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+  if statement.upcase= true
+    return "NO, NOT SINCE 1938!"
+  elsif statement== "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "HUH!? SPEAK UP, SONNY!"
+    return "HUH!? SPEAK UP, SONNY!"
   end
 end
  
